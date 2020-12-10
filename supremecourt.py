1cce736d1199b6b836b5895cbf72ce98ec48e549
Brian of London <bol@snnsite.com>
2020-12-10 18:41:32 +0200

    All files downloaded

--- supremecourt.py
+++ supremecourt.py
@@ -1,9 +1,14 @@
 import requests
+import urllib.request
 import json
 import os
+from pathlib import Path
+from pathvalidate import sanitize_filename
+
 
 outFol = 'supcourt'
 outFile = 'docket.json'
+
 outFileJson = os.path.join(outFol,outFile)
 
 
@@ -27,8 +32,36 @@ else:
 
 proceedings = []
 
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+headers={'User-Agent':user_agent,} 
+
 for data in rjs['ProceedingsandOrder']:
     if 'Links' in data:
         for link in data['Links']:
-            print(link['File'])
-            print(link['DocumentUrl'])
+            fileN = sanitize_filename(link['File'])
+            url = link['DocumentUrl']
+            outF = os.path.join(outFol,fileN)
+            try:
+                print(fileN,url)
+                req = urllib.request.Request(url,None,headers)
+                response = urllib.request.urlopen(req)
+                data = response.read()
+                with open(outF, 'wb') as fl:
+                    fl.write(data)
+            except OSError as err:
+                print(f'fail  {err}')
+                pass
+
+# user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+# headers={'User-Agent':user_agent,} 
+# url = 'https://www.supremecourt.gov/DocketPDF/22/22O155/163215/20201209144914203_2020.12.09%20Certificate%20of%20Service.pdf'
+# req = urllib.request.Request(url,None,headers) #The assembled request
+        
+# response = urllib.request.urlopen(req)
+# data = response.read()
+# f = open('test.pdf','wb')
+# f.write(data)
+# f.close
+
+
+
